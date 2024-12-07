b3af32c14884cd172cc5f08bdb371d39695ac9e7
Pegas1kk <sw.pegasik@gmail.com>
2024-12-07 16:03:04 +0300

    Update задание 4.py

--- задание 4.py
+++ задание 4.py
@@ -1,1 +1,13 @@
-вообще в душе не чаю, как это можно сделать, аж голова заболела после двух часов рассуждений
+num1 = int(input('введите число'))
+roman_dict = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C'}
+keys = list(roman_dict)
+symbols = list(roman_dict.values())
+i = 8
+num2 = ""
+while num1 != 0:
+    if keys[i] <= num1:
+        num2 += symbols[i]
+        num1 -= keys[i]
+    else:
+        i -= 1
+print('ваше число:',num2)
